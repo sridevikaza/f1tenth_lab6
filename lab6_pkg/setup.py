6c56f6639aad22568b4716d4a25d8e4cada9d814
Dvij Kalaria <dvij.kalaria@gmail.com>
2023-10-10 11:43:07 -0400

    lab7->6 rename

--- lab6_pkg/setup.py
+++ lab6_pkg/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'lab7_pkg'
+package_name = 'lab6_pkg'
 
 setup(
     name=package_name,
@@ -15,12 +15,12 @@ setup(
     zip_safe=True,
     maintainer='zzangupenn',
     maintainer_email='zzang@seas.upenn.edu',
-    description='f1tenth lab7_pkg',
+    description='f1tenth lab6_pkg',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rrt_node = lab7_pkg.rrt_node:main',
+            'rrt_node = lab6_pkg.rrt_node:main',
         ],
     },
 )
